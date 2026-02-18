d16b815139775466c0b78fa193e6d1c0f3071fd4
Saipavan911 <saipavan911@gmail.com>
2026-02-18 14:01:19 +0530

    v1.0.1 - Smart Download Organizer working version with logging

---
+++ organizer.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+f = os.listdir("C:/Users/Saipa/OneDrive/Desktop/Dummy_Downloads")
+
+file_types = {
+    ".pdf":"PDF_files",
+    ".jpeg":"Images",
+    ".doc":"Documents"
+    }
+
+
+for file in f:
+    for ext in file_types:
+        if file.endswith(ext):
+            folder_name = file_types[ext]
+            source_path = os.path.join('C:/Users/Saipa/OneDrive/Desktop/Dummy_Downloads/',file)
+            target_folder_path = os.path.join('C:/Users/Saipa/OneDrive/Desktop/Test_download',folder_name)
+            
+            destination_file_path = os.path.join(target_folder_path,file)
+            if not os.path.exists(target_folder_path):
+                os.mkdir(target_folder_path)
+                print(f"Created folder: {target_folder_path}")
+
+
+            shutil.move(source_path,destination_file_path)
+            print(f"Moved '{file}' from '{source_path}' to '{destination_file_path}'")
+
+            break
+    
+                
+            
+                
